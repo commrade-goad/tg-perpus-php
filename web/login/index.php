462abab94a5e6c8fdc513783e3017719a851bf74
Matsumoto B <sayapinterbuatakun@gmail.com>
2024-10-30 22:40:18 +0700

    Perbaikan Login Page

--- web/login/index.php
+++ web/login/index.php
@@ -13,7 +13,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="bg-blue-500 font-bold text-center text-4xl p-5 border-b-4">Perpustakaan UKDC</div>
     <div class="bg-blue-500 h-screen flex-row text-center content-center">
         <div>
             <h1 class="p-5 text-3xl font-bold text-white">
@@ -21,29 +20,28 @@
             </h1>
         </div>
         <form action="/login/proses.php" method="POST">
-        <div>
-            
-            <label for="npm"></label>
-            <input type="text" name="npm" id="npm" class="bg-blue-200 w-1/8 p-2 text-base border text-xl 
-            rounded-xl border-white border-4 focus:border-white focus:outline-none" placeholder="NIM">
-        </div>
-        <div>
-            <label for="password"></label>
-            <input type="password" name="password" id="password" class="bg-blue-200 w-1/8 p-2 text-base border text-xl 
-            rounded-xl border-white border-4 focus:border-white focus:outline-none mt-3" placeholder="Password">
-            <div class="class">
-                <label for="showPassword">
-                    <input type="checkbox" id="showPassword">
-                    Tampilkan Kata Sandi
-                </label>
+            <div>
+                <label for="npm"></label>
+                <input type="text" name="npm" id="npm" class="bg-blue-200 w-1/8 p-2 text-base border text-xl 
+                rounded-xl border-white border-4 focus:border-white focus:outline-none shadow-lg" placeholder="NIM">
             </div>
-        </div>
-        <button type="submit" class="bg-white text-blue-500 font-bold py-2 px-4 rounded mt-3">
-            Login
-        </button>
+            <div>
+                <label for="password"></label>
+                <input type="password" name="password" id="password" class="bg-blue-200 w-1/8 p-2 text-base border text-xl 
+                rounded-xl border-white border-4 focus:border-white focus:outline-none mt-3 shadow-lg" placeholder="Password">
+                <div class="p-5">
+                    <label for="showPassword" class="text-white font-medium">
+                        <input type="checkbox" id="showPassword" class="w-4 h-4"> Tampilkan Kata Sandi
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="bg-white text-blue-500 font-bold py-2 px-4 rounded mt-3 
+            transition transform hover:scale-105 hover:shadow-lg">
+                Login
+            </button>
         </form>
     </div>
-    <div class="bg-blue-500 font-bold text-center text-2xl p-5 border-t-4">@Copyright UKDC IF23</div>
+    <div class="bg-blue-500 font-bold text-center text-2xl p-5 border-t-4 ">@Copyright UKDC IF23</div>
 
     <script>
         const showPassword = document.getElementById('showPassword');
