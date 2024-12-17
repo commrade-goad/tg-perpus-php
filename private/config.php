1973d3712ae5ee1170be0d4b2846e25dc5851b5d
Fernando Perry <fernandoperry1234@gmail.com>
2024-12-17 13:06:12 +0700

    some env support

--- private/config.php
+++ private/config.php
@@ -1,5 +1,17 @@
 <?php
 
+// get ENV
+$env =getenv();
+
 // PATH TO DB
 $db_path = "/home/fernando/git/tg-perpus-php/private/db.sqlite";
 $simp_key = "U0FZQSBBRE1JTgo=";
+
+// check env
+if (isset($env["DB_PATH"])) {
+    $db_path = $env["DB_PATH"];
+}
+
+if (isset($env["ROOT_PASSWORD"])) {
+    $simp_key = $env["ROOT_PASSWORD"];
+}
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/env sh
 
+# now its support taking the path and root password from env
+# so it can be changed on the fly without changing `private/config.php`
+
 cd ./web/
-php -S localhost:8081
+ROOT_PASSWORD="1" \
+    DB_PATH="/home/fernando/git/tg-perpus-php/private/db.sqlite"\
+    php -S localhost:8081
